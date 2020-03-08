9466bd644abd7979a32eff59fcdd67e891564180
Marcia <marciaadler718@gmail.com>
2020-03-08 16:26:10 -0400

    updated portfolio image links in portfolioitems.json

--- portfolio/src/components/PortfolioItems.js
+++ portfolio/src/components/PortfolioItems.js
@@ -1,39 +1,43 @@
 import React from "react";
 
 const styles = {
-  card: {}
+  images: { height: "200px" }
 };
 function PortfolioItems(props) {
   return (
-    <div className="card" style={styles.card}>
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+    <div className="row">
+      <div className="col-12 col-md-3 col-lg-3 d-flex justify-content-center align-items-center">
+        <div className="img-container">
+          <img style={styles.images} alt={props.name} src={props.image} />
+        </div>
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name: </strong>
-            {props.name}
-          </li>
-          <li>
-            <strong>
-              Click <a href={props.link}>Here</a> to view app
-            </strong>
-          </li>
-          <li>
-            <strong>
-              Click <a href={props.github}>Here</a> to view repo on Github
-            </strong>
-          </li>
-          <li>
-            <strong>Description: </strong>
-            {props.description}
-          </li>
-          <li>
-            <strong>Technologies used: </strong>
-            {props.technologies}
-          </li>
-        </ul>
+      <div className="col-12 col-md-8 col-lg-8">
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name: </strong>
+              {props.name}
+            </li>
+            <li>
+              <strong>
+                Click <a href={props.link}>Here</a> to view app
+              </strong>
+            </li>
+            <li>
+              <strong>
+                Click <a href={props.github}>Here</a> to view repo on Github
+              </strong>
+            </li>
+            <li>
+              <strong>Description: </strong>
+              {props.description}
+            </li>
+            <li>
+              <strong>Technologies used: </strong>
+              {props.technologies}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
