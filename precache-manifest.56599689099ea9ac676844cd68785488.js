d239fdb6eb6d2eae132977b1f7cefd658e040765
Marcia <marciaadler718@gmail.com>
2020-03-10 17:45:29 -0400

    Updates

---
+++ precache-manifest.56599689099ea9ac676844cd68785488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f887221e15f77a1cf60845583f5e1b0",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "28697e6df126819f687e",
+    "url": "/ReactPortfolio/static/css/main.0c456daa.chunk.css"
+  },
+  {
+    "revision": "741b1ce9ab0751a86f93",
+    "url": "/ReactPortfolio/static/js/2.56da070f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ReactPortfolio/static/js/2.56da070f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28697e6df126819f687e",
+    "url": "/ReactPortfolio/static/js/main.aae205e0.chunk.js"
+  },
+  {
+    "revision": "8df72df8aea071ac8231",
+    "url": "/ReactPortfolio/static/js/runtime-main.b44af5e4.js"
+  },
+  {
+    "revision": "01980763ec93db5255d87cbd9266f19d",
+    "url": "/ReactPortfolio/static/media/backgroundimage.01980763.jpg"
+  },
+  {
+    "revision": "16d51c9bf9ac3836bdc77948d27fdc5e",
+    "url": "/ReactPortfolio/static/media/me.16d51c9b.jpeg"
+  }
+]);
